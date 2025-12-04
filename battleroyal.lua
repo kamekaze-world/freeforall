a558385c515eb7b6b1c7c5d3b24f2c0549d06cdb
Kamekaze <ilive@kamekaze.world>
2025-12-04 07:20:03 -0500

    Add files via upload

---
+++ battleroyal.lua
@@ -0,0 +1,82 @@
+local tt = gameOption("Common.States")
+table.insert(tt, "external/mods/kame_isuka.zss")
+modifyGameOption("Common.States", tt)
+
+main.t_itemname.freeforallcpu= function()
+		main.f_playerInput(main.playerInput, 1)
+		main.t_pIn[2] = 1
+		--main.lifebar.p1score = true
+		--main.lifebar.p2ailevel = true
+		main.orderSelect[1] = false
+		main.orderSelect[2] = false
+		main.selectMenu[2] = true
+		main.stageMenu = true
+		main.teamMenu[1].ratio = false
+		main.teamMenu[1].simul = true
+		main.teamMenu[1].single = false
+		main.teamMenu[1].tag = false
+		main.teamMenu[1].turns = false
+		main.teamMenu[2].ratio = false
+		main.teamMenu[2].simul = true
+		main.teamMenu[2].single = false
+		main.teamMenu[2].tag = false
+		main.teamMenu[2].turns = false
+		main.versusScreen = true
+		main.victoryScreen = true
+		main.txt_mainSelect:update({text = motif.select_info.title_freeforallbattle_text})
+		setGameMode('freeforall')
+		modifyGameOption('Options.Team.PowerShare', 0)
+		hook.run("main.t_itemname")
+		return start.f_selectMode
+	end
+	
+	main.t_itemname.freeforall=function()
+		setHomeTeam(1)
+		main.coop = true
+		main.cpuSide[2] = false
+		--main.lifebar.p1wincount = true
+		--main.lifebar.p2wincount = true
+		main.numSimul = {2, math.min(4, math.max(2, math.ceil(gameOption('Config.Players') / 2)))}
+		main.numTag = {2, math.min(4, math.max(2, math.ceil(gameOption('Config.Players') / 2)))}
+		main.selectMenu[2] = true
+		main.stageMenu = true
+		main.teamMenu[1].simul = true
+		main.teamMenu[1].tag = false
+		main.teamMenu[2].simul = true
+		main.teamMenu[2].tag = false
+		main.versusScreen = true
+		main.victoryScreen = true
+		main.txt_mainSelect:update({text = motif.select_info.title_versuscoop_text})
+		setGameMode('freeforall')
+		modifyGameOption('Options.Team.PowerShare', 0)
+		hook.run("main.t_itemname")
+		return start.f_selectMode
+	end
+	
+		main.t_itemname.freeforallwatch=function()
+		setHomeTeam(1)
+		main.f_playerInput(main.playerInput, 1)
+		main.t_pIn[2] = 1
+		main.cpuSide[1] = true
+		--main.lifebar.p1ailevel = true
+		--main.lifebar.p2ailevel = true
+		main.selectMenu[2] = true
+		main.stageMenu = true
+		main.teamMenu[1].ratio = false
+		main.teamMenu[1].simul = true
+		main.teamMenu[1].single = false
+		main.teamMenu[1].tag = false
+		main.teamMenu[1].turns = false
+		main.teamMenu[2].ratio = false
+		main.teamMenu[2].simul = true
+		main.teamMenu[2].single = false
+		main.teamMenu[2].tag = false
+		main.teamMenu[2].turns = false
+		main.versusScreen = true
+		main.victoryScreen = true
+		main.txt_mainSelect:update({text = motif.select_info.title_watch_text})
+		setGameMode('freeforall')
+		modifyGameOption('Options.Team.PowerShare', 0)
+		hook.run("main.t_itemname")
+		return start.f_selectMode
+	end
