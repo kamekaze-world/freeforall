5928d9d15109512d002f13d0b931aa012a9e285b
Kamekaze <ilive@kamekaze.world>
2026-01-17 13:21:55 -0500

    Update battleroyal_new.lua

--- battleroyal_new.lua
+++ battleroyal_new.lua
@@ -2,15 +2,15 @@ local tt = gameOption("Common.States")
 table.insert(tt, "external/mods/kame_isuka.zss")
 modifyGameOption("Common.States", tt)
 
-kkff = {}
+ffa = {}
+
+ffa.sd=loadIni('external/mods/def/ffa.def')
 
 main.t_itemname.freeforallcpu= function()
-		main.f_playerInput(main.playerInput, 1)
+main.f_playerInput(main.playerInput, 1)
 		main.t_pIn[2] = 1
 		--main.lifebar.p1score = true
 		--main.lifebar.p2ailevel = true
-	    main.numSimul = {2, math.min(4, gameOption('Config.Players'))}
-		main.numTag = {2, math.min(4, gameOption('Config.Players'))}
 		main.motif.versusscreen = true
 		main.motif.victoryscreen = true
 		main.orderSelect[1] = false
@@ -27,14 +27,12 @@ main.t_itemname.freeforallcpu= function()
 		main.teamMenu[2].single = false
 		main.teamMenu[2].tag = false
 		main.teamMenu[2].turns = false
-		main.versusScreen = true
-		main.victoryScreen = true
-		textImgSetText(motif.select_info.title.TextSpriteData, kkff.select_info.title.text.freebattle)
+		textImgSetText(motif.select_info.title.TextSpriteData, ffa.sd.select_info.title.text.freeforall)
 		setGameMode('freeforall')
 		modifyGameOption('Options.Team.PowerShare', 0)
 		hook.run("main.t_itemname")
-
 		return start.f_selectMode
+
 	end
 	
 	main.t_itemname.freeforall=function()
@@ -43,29 +41,19 @@ main.t_itemname.freeforallcpu= function()
 		main.cpuSide[2] = false
 		--main.lifebar.p1wincount = true
 		--main.lifebar.p2wincount = true
-		main.numSimul = {2, math.min(4, gameOption('Config.Players'))}
-		main.numTag = {2, math.min(4, gameOption('Config.Players'))}
-		main.motif.versusscreen = true
-		main.motif.victoryscreen = true
-		main.orderSelect[1] = false
-		main.orderSelect[2] = false
+		main.numSimul = {2, math.min(4, math.max(2, math.ceil(gameOption('Config.Players') / 2)))}
+		main.numTag = {2, math.min(4, math.max(2, math.ceil(gameOption('Config.Players') / 2)))}
 		main.selectMenu[2] = true
 		main.stageMenu = true
-		main.teamMenu[1].ratio = false
 		main.teamMenu[1].simul = true
-		main.teamMenu[1].single = false
 		main.teamMenu[1].tag = false
-		main.teamMenu[1].turns = false
-		main.teamMenu[2].ratio = false
 		main.teamMenu[2].simul = true
-		main.teamMenu[2].single = false
 		main.teamMenu[2].tag = false
-		main.teamMenu[2].turns = false
-		main.versusScreen = true
-		main.victoryScreen = true
-		textImgSetText(motif.select_info.title.TextSpriteData, kkff.select_info.title.text.freebattle)
+		main.motif.versusscreen = true
+		main.motif.victoryscreen = true
+		textImgSetText(motif.select_info.title.TextSpriteData, ffa.sd.select_info.title.text.freeforallcpu)
+		setGameMode('freeforall')
 		modifyGameOption('Options.Team.PowerShare', 0)
-	setGameMode('freeforall')
 		hook.run("main.t_itemname")
 		return start.f_selectMode
 	end
@@ -77,12 +65,6 @@ main.t_itemname.freeforallcpu= function()
 		main.cpuSide[1] = true
 		--main.lifebar.p1ailevel = true
 		--main.lifebar.p2ailevel = true
-		main.numSimul = {2, math.min(4, gameOption('Config.Players'))}
-		main.numTag = {2, math.min(4, gameOption('Config.Players'))}
-		main.motif.versusscreen = true
-		main.motif.victoryscreen = true
-		main.orderSelect[1] = false
-		main.orderSelect[2] = false
 		main.selectMenu[2] = true
 		main.stageMenu = true
 		main.teamMenu[1].ratio = false
@@ -95,12 +77,11 @@ main.t_itemname.freeforallcpu= function()
 		main.teamMenu[2].single = false
 		main.teamMenu[2].tag = false
 		main.teamMenu[2].turns = false
-		main.versusScreen = true
-		main.victoryScreen = true
-		textImgSetText(motif.select_info.title.TextSpriteData, kkff.select_info.title.text.freebattle)
+		main.motif.versusscreen = true
+		main.motif.victoryscreen = true
+		textImgSetText(motif.select_info.title.TextSpriteData, ffa.sd.select_info.title.text.freeforallwatch)
 		setGameMode('freeforall')
 		modifyGameOption('Options.Team.PowerShare', 0)
 		hook.run("main.t_itemname")
 		return start.f_selectMode
-
 	end
